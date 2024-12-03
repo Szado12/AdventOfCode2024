4e252cca4129ff8e5d07b221dc5d12e31fb92119
Szado12 <michal.pijanowski2@gmail.com>
2024-12-04 00:10:55 +0100

    day 1

---
+++ AdventOfCode/Day1-Part1/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string path = "../../../input.txt";
+
+var lines = File.ReadAllLines(path);
+
+var maxIndex = lines.Length;
+
+List<int> first = new List<int>(maxIndex);
+List<int> second = new List<int>(maxIndex);
+
+foreach (var line in lines)
+{
+    var numbers = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    first.Add(int.Parse(numbers[0]));
+    second.Add(int.Parse(numbers[1]));
+}
+
+first.Sort();
+second.Sort();
+long diff = 0;
+
+for (int i = 0; i < maxIndex; i++)
+{
+    diff += long.Abs(first[i]-second[i]);
+}
+
+Console.WriteLine(diff);
---
+++ AdventOfCode/Day1-Part2/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string path = "../../../input.txt";
+
+var lines = File.ReadAllLines(path);
+
+var maxIndex = lines.Length;
+
+List<int> first = new List<int>(maxIndex);
+Dictionary<int, int> second = new Dictionary<int, int>();
+
+foreach (var line in lines)
+{
+    var numbers = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    first.Add(int.Parse(numbers[0]));
+    var secondNumber = int.Parse(numbers[1]);
+
+    if (second.ContainsKey(secondNumber))
+        second[secondNumber]++;
+    else
+        second[secondNumber] = 1;
+}
+
+
+long diff = 0;
+
+foreach (var firstNumber in first)
+{
+    if (!second.ContainsKey(firstNumber))
+        continue;
+    
+    diff += firstNumber * second[firstNumber];
+}
+
+Console.WriteLine(diff);
