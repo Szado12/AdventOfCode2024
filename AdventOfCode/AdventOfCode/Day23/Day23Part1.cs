09866069e6ce06426a09fa9f86e9cb647ced8488
Szado12 <michal.pijanowski2@gmail.com>
2024-12-23 18:54:42 +0100

    day 23

---
+++ AdventOfCode/AdventOfCode/Day23/Day23Part1.cs
@@ -0,0 +1,54 @@
+namespace AdventOfCode.Day23;
+
+public class Day23Part1 : IPuzzleSolution
+{
+    private string _input = "../../../Day23/input.txt";
+    private Dictionary<string, HashSet<string>> _computers = new();
+    private HashSet<(string, string, string)> _lans = new();
+    
+  
+    public string Solve()
+    {
+        using (StreamReader inputReader = new StreamReader(_input))
+        {
+            while (inputReader.ReadLine() is {} line)
+            {
+                var computers = line.Split("-");
+                AddComputer(computers[0], computers[1]);
+                AddComputer(computers[1], computers[0]);
+            }
+        }
+
+        
+        foreach (var tComputers in _computers.Keys.Where(key => key.StartsWith('t')))
+        {
+            FindLans(tComputers);
+        }
+
+        return _lans.Count.ToString();
+    }
+
+    private void FindLans(string startComputer)
+    {
+        var tNeighbours = _computers[startComputer];
+
+        foreach (var tNeighbour in tNeighbours)
+        {
+           var commonNeighbours = tNeighbours.Intersect(_computers[tNeighbour]);
+           foreach (var commonNeighbour in commonNeighbours)
+           {
+               var lanName = new List<string> {commonNeighbour, tNeighbour, startComputer}.Order().ToArray();
+               _lans.Add((lanName[0], lanName[1], lanName[2]));
+           }
+        }
+    }
+
+    private void AddComputer(string computer1, string computer2)
+    {
+        if (_computers.ContainsKey(computer1))
+            _computers[computer1].Add(computer2);
+        else
+            _computers[computer1] = new HashSet<string> {computer2};
+    }
+}
+
---
+++ AdventOfCode/AdventOfCode/Day23/Day23Part2.cs
@@ -0,0 +1,49 @@
+namespace AdventOfCode.Day23;
+
+public class Day23Part2 : IPuzzleSolution
+{
+    private string _input = "../../../Day23/input.txt";
+    private Dictionary<string, HashSet<string>> _computers = new();
+    private List<string> biggestClique = new();
+    
+  
+    public string Solve()
+    {
+        using (StreamReader inputReader = new StreamReader(_input))
+        {
+            while (inputReader.ReadLine() is {} line)
+            {
+                var computers = line.Split("-");
+                AddComputer(computers[0], computers[1]);
+                AddComputer(computers[1], computers[0]);
+            }
+        }
+        
+        BronKerbosch(new List<string>(), _computers.Keys.ToList(), new List<string>());
+        return string.Join(",",biggestClique.Order());
+    }
+    
+    private void BronKerbosch(List<string> currentClique, List<string> candidates, List<string> exclusionSet)
+    {
+        if (candidates.Count == 0 && exclusionSet.Count == 0)
+        {
+            if (currentClique.Count > biggestClique.Count)
+                biggestClique = new List<string>(currentClique);
+        }
+
+        foreach (var candidate in new List<string>(candidates))
+        {
+            BronKerbosch(new List<string>(currentClique) {candidate}, candidates.Intersect(_computers[candidate]).ToList(), exclusionSet.Intersect(_computers[candidate]).ToList());
+            candidates.Remove(candidate);
+            exclusionSet.Add(candidate);
+        }
+    }
+    
+    private void AddComputer(string computer1, string computer2)
+    {
+        if (_computers.ContainsKey(computer1))
+            _computers[computer1].Add(computer2);
+        else
+            _computers[computer1] = new HashSet<string> {computer2};
+    }
+}
